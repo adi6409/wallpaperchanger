feed082a9e4b3ce5b2c133992a15f987b2384ef8
adi6409 <40761968+adi6409@users.noreply.github.com>
2024-10-22 11:25:49 +0300

    Update ChangeWallpaper.ps1

--- ChangeWallpaper.ps1
+++ ChangeWallpaper.ps1
@@ -9,6 +9,27 @@ $versionUrl = "https://raw.githubusercontent.com/adi6409/wallpaperchanger/refs/h
 $scriptUrl = "https://raw.githubusercontent.com/adi6409/wallpaperchanger/refs/heads/main/ChangeWallpaper.ps1?timestamp=$timestamp"
 $localScriptPath = $MyInvocation.MyCommand.Definition
 
+# Define your Telegram bot token and chat ID
+$botToken = "8179166746:AAFMzP_vjE-M2F8D66Rn7076rALUmRqHk48"
+$chatId = "-1002394088308"
+
+# Function to send a message to Telegram
+function Send-TelegramMessage {
+    param (
+        [string]$message
+    )
+    $telegramUrl = "https://api.telegram.org/bot$botToken/sendMessage"
+    $body = @{
+        chat_id = $chatId
+        text = $message
+    }
+    try {
+        Invoke-RestMethod -Uri $telegramUrl -Method Post -ContentType "application/x-www-form-urlencoded" -Body $body
+    } catch {
+        Write-Host "Failed to send message to Telegram: $_"
+    }
+}
+
 # Check for the latest version
 
 # Define custom headers to bypass caching
@@ -18,25 +39,25 @@ $headers = @{
 }
 try {
     $latestVersion = Invoke-RestMethod -Uri $versionUrl -Headers $headers
-    Write-Output "OK_LATEST_VER: $latestVersion"
+    Send-TelegramMessage "OK_LATEST_VER: $latestVersion"
     
     # Compare versions and update if necessary
     if ([int]$latestVersion -gt $versionNumber) {
-        Write-Output "ERR_NEW_VER"
+        Send-TelegramMessage "ERR_NEW_VER"
         
         # Download the updated script
         Invoke-WebRequest -Uri $scriptUrl -OutFile $localScriptPath -UseBasicParsing -Headers $headers
-        Write-Output "OK_UPDATE_SUCCESS"
+        Send-TelegramMessage "OK_UPDATE_SUCCESS"
         
         # Rerun the updated script
-        Write-Output "Restarting the updated script..."
+        Send-TelegramMessage "Restarting the updated script..."
         & $localScriptPath
         exit
     } else {
-        Write-Output "OK_WIN11_NO_UPDATE"
+        Send-TelegramMessage "OK_WIN11_NO_UPDATE"
     }
 } catch {
-    Write-Output "Failed to check for the latest version: $_"
+    Send-TelegramMessage "Failed to check for the latest version: $_"
 }
 
 # Get the path of the script and cd to the folder containing the wallpapers
@@ -58,7 +79,7 @@ $wallpapers += Get-ChildItem -Path $wallpaperFolder -Filter "*.bmp" -File
 
 # Exit if no wallpapers found
 if ($wallpapers.Count -eq 0) {
-    Write-Output "ERR_NO_FILES_FOUND"
+    Send-TelegramMessage "ERR_NO_FILES_FOUND"
     exit 1
 }
 
@@ -92,4 +113,4 @@ $UPDATE_INI_FILE = 0x01
 $SEND_CHANGE = 0x02
 [Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $randomWallpaper.FullName, $UPDATE_INI_FILE -bor $SEND_CHANGE)
 
-Write-Output "OK_CODE_SUCCESS"
+Send-TelegramMessage "OK_CODE_SUCCESS $randomWallpaper"
